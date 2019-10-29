f67ec6829c4d3548eb7914ec2a198efd37ab30b5
vkhalayim <vasyl.khalaim@ukt.net>
2019-10-29 17:27:34 +0200

    Three exercises done

---
+++ practical_introduction_book/chapter6/ex3.py
@@ -0,0 +1,19 @@
+"""
+
+insertion sort
+
+"""
+
+arr = [5, 2, 1, 8, 8]
+
+for i in range(1, len(arr)):
+
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and key < arr[j]:
+        arr[j + 1] = arr[j]
+        j -=1
+    arr[j + 1] = key
+
+print(arr)
---
+++ practical_introduction_book/chapter6/ex5.py
@@ -0,0 +1,11 @@
+"""
+
+Write a program that asks the user to enter a string, then prints out each letter of the string
+doubled and on a separate line
+
+"""
+
+userString = input("Please, enter something: ")
+
+for i in userString:
+    print(i * 2)
